90e109d8c8156af6786e69e63bc0df594c453284
aidinabedi <aidinabedi@gmail.com>
2019-08-22 00:24:20 +0200

    fix issue checking navigator user-agent in node.js

--- src/resources/audio.js
+++ src/resources/audio.js
@@ -3,19 +3,21 @@ Object.assign(pc, function () {
 
     // checks if user is running IE
     var ie = (function () {
-        var ua = window.navigator.userAgent;
+        if (typeof navigator !== 'undefined') {
+            var ua = navigator.userAgent;
 
-        var msie = ua.indexOf('MSIE ');
-        if (msie > 0) {
-            // IE 10 or older => return version number
-            return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
-        }
+            var msie = ua.indexOf('MSIE ');
+            if (msie > 0) {
+                // IE 10 or older => return version number
+                return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
+            }
 
-        var trident = ua.indexOf('Trident/');
-        if (trident > 0) {
-            // IE 11 => return version number
-            var rv = ua.indexOf('rv:');
-            return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
+            var trident = ua.indexOf('Trident/');
+            if (trident > 0) {
+                // IE 11 => return version number
+                var rv = ua.indexOf('rv:');
+                return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
+            }
         }
 
         return false;
