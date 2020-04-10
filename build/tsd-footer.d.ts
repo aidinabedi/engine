a99e26b2296369c9c9d1ed104ea52fed7e3b1ee0
aidinabedi <aidinabedi@gmail.com>
2020-04-10 02:48:57 +0200

    add pc.ScriptComponent#get<T>

--- build/tsd-footer.d.ts
+++ build/tsd-footer.d.ts
@@ -1,1 +1,12 @@
+declare namespace pc {
+    interface ScriptComponent {
+        /**
+         * @description Get a script instance (if attached).
+         * @param type - The type of {@link pc.ScriptType}.
+         * @returns If script is attached, the instance is returned. Otherwise null is returned.
+         */
+        get<T extends pc.ScriptType>(type: { new(): T }): T | null;
+    }
+}
+
 export = pc;
